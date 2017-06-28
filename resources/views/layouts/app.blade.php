04585273a682f402430ea8ea8843948a60ffbc4a
Neo Ighodaro <neo@creativitykills.co>
2017-06-28 21:25:03 +0100

    Updated the app to remove innecessary spaces

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,12 +24,11 @@
     @yield('styles')
 </head>
 <body>
-    {{-- Google Tag Manager --}}
     @if (config('services.google_tag_manager_id'))
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id={{ config('services.google_tag_manager_id') }}"
-            height="0" width="0" style="display:none;visibility:hidden"></iframe>
-        </noscript>
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer', "{{ config('services.google_tag_manager.id') }}");</script>
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id={{ config('services.google_tag_manager_id') }}"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe>
+    </noscript>
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer', "{{ config('services.google_tag_manager.id') }}");</script>
     @endif
 
     <div id="app">
@@ -39,7 +38,6 @@
     </div>
 
     @include('partials.popups')
-
     <script src="{{ mix('js/app.js') }}"></script>
     @yield('scripts')
 </body>
