940dc4ef80ba7c4beb3a3ed2f61afbb7c7930ce6
Neo Ighodaro <neo@creativitykills.co>
2017-07-25 08:27:11 +0100

    Minor fix

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -5,6 +5,6 @@
             <li><a href="#" title="About">Privacy Policy</a></li>
             <li><a href="#" title="About">Site map</a></li>
         </ul>--}}
-        <span class="closer">Crafted with &#10084; at <a href="https://creativitykills.co" target="_blank" title="Nigerian web design company">CreativityKills Co.</a> // <a href="https://github.com/laravelnigeria/website" title="Laravel Nigeria Websites Source Code" target="_blank">Src Code</a></span>
+        <span class="closer">Crafted with &#10084; at <a href="https://creativitykills.co" target="_blank" title="Nigerian web design company">CreativityKills Co.</a> // <a href="https://github.com/laravelnigeria/website" title="Laravel Nigeria Websites Source Code" target="_blank">Source Code</a></span>
     </div>
 </footer>
