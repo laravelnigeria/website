f046f8e8338e80d4d500fb7c1940275f6892632c
Neo Ighodaro <neo@creativitykills.co>
2017-07-25 09:15:31 +0100

    Added a deploy script for production

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+composer install --no-interaction --no-dev
+
+php artisan optimize
+php artisan migrate
+
+php artisan config:clear
+php artisan config:cache
+
+php artisan route:clear
+php artisan route:cache
