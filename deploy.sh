a5d51f2d3e1a23285223e112d1ff2f8ccc8df705
Neo Ighodaro <neo@creativitykills.co>
2018-12-21 14:43:58 +0100

    minor update to deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,10 @@ composer install --no-interaction --no-dev
 php artisan optimize
 php artisan migrate --force
 
+php artisan cache:clear
+
 php artisan config:clear
 php artisan config:cache
 
 php artisan route:clear
 php artisan route:cache
-
-php artisan cache:clear
