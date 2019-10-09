d2074811f06a06c1b82a3e720678b0b64fe1d96e
Neo Ighodaro <neo@creativitykills.co>
2019-10-09 10:24:52 +0200

    Update web.php

--- 2019/routes/web.php
+++ 2019/routes/web.php
@@ -5,4 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/optin/verify/{id}/{hash}', 'RegistrationWebhookController@verify')->name('sponsor.optin.verify');
 Route::post(config('tito.webhook_url'), 'RegistrationWebhookController@handle');
 Route::get('/code-of-conduct', 'CodeOfConductController')->name('code-of-conduct');
+Route::redirect('/cfp', 'https://laravelnigeria.typeform.com/to/rosG3K');
 Route::get('/', 'HomeController')->name('index');
